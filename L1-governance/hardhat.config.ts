907b05e7e2fbbbf3c4b2616c4c89beb439b48e03
bristinborah <bristin.borah.7@gmail.com>
2023-10-08 18:18:50 +0530

    ZkSync crosschain call

---
+++ L1-governance/hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+// import file with Göerli params
+const goerli = require("./goerli.json");
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.13",
+  },
+  networks: {
+    // Göerli network
+    goerli: {
+      url: goerli.nodeUrl,
+      accounts: [goerli.deployerPrivateKey],
+    },
+  },
+};
+
+export default config;
+
